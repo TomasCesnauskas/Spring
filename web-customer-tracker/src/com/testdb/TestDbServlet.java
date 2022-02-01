8570d76dc57b27a44dcd86a6683a2100d79777cf
TomasCesnauskas <cesnauskas.tomas@gmail.com>
2022-02-01 21:26:03 +0200

    Spring 26

--- web-customer-tracker/src/com/testdb/TestDbServlet.java
+++ web-customer-tracker/src/com/testdb/TestDbServlet.java
@@ -6,12 +6,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 @WebServlet("/TestDbServlet")
 public class TestDbServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        //       super.doGet(req, resp);
+//        setup connection variables
+        String user = "root";
+        String pass = "admin";
+        String jdbcUrl = "jdbc:mysql://localhost:3306/db?useSSL=false";
+        String driver = "com.mysql.cj.jdbc.Driver";
+
+//        get connection to database
+        try {
+//            spausdinsim i narsykle
+            PrintWriter printWriter = resp.getWriter();
+            printWriter.println("Connecting to database: " + jdbcUrl);
+            Class.forName(driver);
+            Connection connection = DriverManager.getConnection(jdbcUrl, user, pass);
+            printWriter.println("Succes.");
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ServletException(e);
+        }
     }
 }
